394e6a5ce6532c436994301bbbf50bac7ecf751a
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-02-03 11:58:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3'); 
-audio.loop=true;
-// audio.play();
+var car = document.querySelector(".car");
+car.addEventListener("click", function()
+                     { var a = new Audio('sound.mp3');
+                      a.loop = true;
+                      a.play();
+                     });
