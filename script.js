0f5a33bbecdd7b6e6f5910d2c9465499bfb96d07
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-02-03 12:02:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var car = document.querySelector(".car");
-car.addEventListener("click", function()
-                     { var a = new Audio('sound.mp3');
-                      a.loop = true;
-                      a.play();
-                     });
